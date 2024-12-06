3a2ccae11144db428e3bb001b8b14b4bfa9c6131
Cesar Hernandez <149346749+cesarurielhr@users.noreply.github.com>
2024-12-05 22:12:46 -0700

    Models Carrito

---
+++ src/models/carritoModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const carritoSchema = new mongoose.Schema({
+    clienteId: { type: String, required: true 
+
+        
+    },
+    items: [{
+        productoId: { type: String, required: true },
+        name: { type: String },
+        price: { type: Number },
+        cantidad: { type: Number, default: 1 },
+        subtotal: { type: Number },
+        iva: { type: Number },
+    }],
+    total: { type: Number, default: 0 }, // Inicializamos en 0
+    status: { type: String, enum: ['Activo', 'Inactivo'], default: 'Activo' },
+    fechaCreacion: { type: String },
+    fechaCierre: { type: String },
+});
+
+// Método para calcular el total antes de guardar el carrito
+carritoSchema.pre('save', function(next) {
+    this.total = this.items.reduce((total, item) => (total + item.subtotal)+(item.iva), 0);
+    next();
+});
+
+module.exports = mongoose.model('Carrito', carritoSchema);
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,10 +1,23 @@
-const mongoose = require('mongoose');
-
-const usuarioSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true }
+const userSchema = new mongoose.Schema({
+    facturapiId: { type: String },
+    Name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    tax_system: { type: String, require: true },
+    address: {
+        street: { type: String, required: true },
+        exterior_number: { type: String, required: true },
+        interior_number: { type: String },
+        neighborhood: { type: String, required: true },
+        zip: { type: String, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        country: { type: String, required: true },
+    },
+    registrationDate: { type: String, require: true },
+    userTipe: { type: String, require: true },
+    rfc: { type: String, require: true },
+    PaymentMethod: { type: String, require: true, enum: ['Tarjeta', 'Paypal', 'Bitcoin', 'Tarjeta de Credito'] }
 });
 
-const Usuario = mongoose.model('Usuarios', usuarioSchema);
-
-module.exports = Usuario;
+const User = mongoose.model('User', userSchema);
